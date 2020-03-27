1794205792c813b257fd14a46e1de2da78097582
arlei duarte <arleidt@yahoo.com.br>
2020-03-26 22:13:41 -0300

    primeira confirmação

---
+++ core/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import PontoTuristico
+
+admin.site.register(PontoTuristico)
+
+# Register your models here.
---
+++ venvApi/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\arlei.duarte\documents\projapiteste\venvapi\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
